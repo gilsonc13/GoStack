91c9ffa6205ceee5cf31579bae600097f9938bcc
Gilson Camargo <gilsonc13@gmail.com>
2020-04-25 13:42:02 -0300

    Model User e Relacionamento
    Criação do Model User.
    Relacionamento com o Model Appointment
    
    Criptografia de senha do User.

--- modulos/gobarber-backend/src/routes/index.ts
+++ modulos/gobarber-backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router, json } from 'express';
 import appointmenstsReouter from './appointments.routes';
+import usersReouter from './users.routes';
 
 const routes = Router();
 
 routes.use(json());
 routes.use('/appointments', appointmenstsReouter);
+routes.use('/users', usersReouter);
 
 export default routes;
--- modulos/gobarber-backend/src/services/CreateAppointmentService.ts
+++ modulos/gobarber-backend/src/services/CreateAppointmentService.ts
@@ -4,12 +4,12 @@ import Appointment from '../models/Appointment';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppoitmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentRepository = getCustomRepository(AppointmentRepository);
 
     const appointmentDate = startOfHour(date);
@@ -23,7 +23,7 @@ class CreateAppoitmentService {
     }
 
     const appointment = appointmentRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
