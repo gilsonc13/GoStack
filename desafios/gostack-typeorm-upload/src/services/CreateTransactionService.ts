7dd3d92155d2e77ab9dfb311c834540f91c5031a
Gilson Camargo <gilsonc13@gmail.com>
2020-04-26 22:02:57 -0300

    Desafio Node - Typeorm
    Desafio Upload de arquivo com Node, Typeorm.

---
+++ desafios/gostack-typeorm-upload/src/services/CreateTransactionService.ts
@@ -0,0 +1,46 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+import CreateCategoryService from './CreateCategoryService';
+import TransactionRespository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  title_category: string;
+}
+
+class CreateTransactionService {
+  public async execute({
+    title,
+    value,
+    type,
+    title_category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRespository);
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('Tipo não permitido!');
+    }
+
+    const createCategory = new CreateCategoryService();
+    const category = await createCategory.execute({
+      title_category,
+    });
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: category.id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
